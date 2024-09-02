0f164415999552b77a7baa2e6886e58a926f057e
Benjamin <kodiugos@gmail.com>
2024-09-01 23:36:18 -0500

    file changes

--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -21,8 +21,36 @@ namespace WeatherSimulator
 
             double average_temp = CalculateAverageTemp(temp);
             Console.WriteLine($"Temperature average is: {CalculateAverageTemp(temp)}");
+            Console.WriteLine($"Maximum temperature is: {temp.Max()}");
+            Console.WriteLine($"Minimum temperature is: {temp.Min()}");
+            Console.WriteLine($"Most Common Conditions is: {MostCommonConditions(conditions)}");
 
             Console.ReadKey();
+        }
+
+        static string MostCommonConditions(string[] conditions)
+        {
+            int count = 0;
+            string most_common = conditions[0];
+            // first interation
+            for(int i = 0; i < conditions.Length; i++)
+            {
+                int temp_count = 0;
+                // for loop in for loop
+                for(int j = 0; j < conditions.Length; j++)
+                {
+                    if(conditions[j] == conditions[i])
+                    {
+                        temp_count++;
+                    }
+                }
+                if(temp_count > count)
+                {
+                    most_common = conditions[i];
+                }
+            }
+            return most_common;
+
         }
 
         static double CalculateAverageTemp(int[] temp_values)
